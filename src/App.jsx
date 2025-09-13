5843299efae8954ac29cf5700f73956a897de2b1
Aamir Raza <merciful9795@gmail.com>
2025-09-13 18:08:07 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=' flex justify-center h-screen bg-green-100'>
+        Hellow world
+      </div>
+    </>
+  )
+}
+
+export default App
